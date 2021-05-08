04e7c528399bff98771a8fbad6f11a2568ace9c5
authoritydmc <com.mailuser@gmail.com>
2021-05-09 01:43:17 +0530

    fix crash

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 import json
-import setup
+import logging
+
 class prod_config:
     sender_email=os.environ.get('email',None)
     DB_URI=os.environ.get('sqlite:///dblocal.sqlite3',None)
@@ -9,11 +10,14 @@ class prod_config:
 
 
 class local_config:
+    try:
+        import setup
         sender_email=setup.email
         DB_URI="sqlite:///dblocal.sqlite3"
         password=setup.password
         environment='local'
+    except:
+        logging.error("Error Occured IN Setting Up local env")
 
-
-
-print(local_config.sender_email)
+ens=prod_config.environment if prod_config.environment!=None else 'Local'
+print("Running in ",ens)
