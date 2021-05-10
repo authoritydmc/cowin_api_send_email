823ad6bca1ac2c8a883265c763ab498b0cc020d4
authoritydmc <com.mailuser@gmail.com>
2021-05-10 10:27:17 +0530

    added UTC support

--- cowin_get_email/databases/vaccine_model.py
+++ cowin_get_email/databases/vaccine_model.py
@@ -1,5 +1,5 @@
 from cowin_get_email.databases.database import Base, engine, Session
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean,Text
 from datetime import datetime
 import json
 
@@ -8,29 +8,17 @@ class Vaccine(Base):
     __tablename__ = 'vaccines'
     id = Column('id', Integer, primary_key=True)
     pincode = Column('pincode', Integer)
-    vaccine = Column('vaccine', String)
-    min_age = Column('min_age', Integer)
-    fee = Column('fee', String)
-    available_vac_cap = Column('available_vaccine_cap', Integer)
-    center_id = Column('center_id', Integer)
-    center_name = Column('center_name', String)
-    center_address = Column('center_address', String)
-    date_avail=Column('date_available', String)
-    prev_cap = Column('prev_cap', Integer, default=-1)
+    res_str=Column(Text)
+    lastUpdated=Column(String)
+
 
 
     def __repr__(self):
      response={}
-     response['vaccine_name']=self.vaccine
      response['pincode']=self.pincode
-     response['min_age']=self.min_age
-     response['fee']=self.fee
-     response['available_capacity']=self.available_vac_cap
-     response['previous_capacity']=self.prev_cap
-     response['center_id']=self.center_id
-     response['center_name']=self.center_name
-     response['center_address']=self.center_address
-     response['date_available']=self.date_avails
+     response['last_updated']=self.lastUpdated
+     response['res_str']=self.res_str
+
      return json.dumps(response,indent=4)
 
      
@@ -38,20 +26,11 @@ class Vaccine(Base):
 
 
 
-def addVaccine(vaccine,pincode, min_age, fee, available_vaccine_cap, center_id, center_name, center_address,date_avail,previous_cap):
+def addVaccine(pincode,res_str,lastUpdated=''):
     try:
         session=Session()
         temp_v=Vaccine()
-        temp_v.vaccine=vaccine
-        temp_v.pincode=int(pincode)
-        temp_v.min_age=int(min_age)
-        temp_v.fee=fee
-        temp_v.available_vac_cap=int(available_vaccine_cap)
-        temp_v.center_id=int(center_id)
-        temp_v.center_name=center_name
-        temp_v.center_address=center_address 
-        temp_v.prev_cap=int(previous_cap)
-        temp_v.date_avail=date_avail
+
         session.add(temp_v)
         session.commit()
         return 'Vaccine Added successfully',True
--- cowin_get_email/utility/common_util.py
+++ cowin_get_email/utility/common_util.py
@@ -1,5 +1,6 @@
 from datetime import  datetime
 import os,time
+from datetime import timezone
 
 
 
@@ -7,3 +8,11 @@ def getDate():
     os.environ['TZ'] = 'Asia/Calcutta'
     time.tzset()
     return datetime.today().strftime('%d-%m-%Y')
+
+def getUtcTimeStamp():
+    dt = datetime.now(timezone.utc)
+    
+    utc_time = dt.replace(tzinfo=timezone.utc)
+    utc_timestamp = utc_time.timestamp()
+    
+    return int(utc_timestamp)
