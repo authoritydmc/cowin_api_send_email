c2000edf8e1d1b3ea6b1a17a9a307b7a0f555c4d
authoritydmc <com.mailuser@gmail.com>
2021-05-10 17:49:24 +0530

    Added pincode util [testing]

---
+++ cowin_get_email/utility/pincode_util.py
@@ -0,0 +1,31 @@
+from cowin_get_email.databases import district_model,pincode_model
+import requests
+from datetime import datetime
+from cowin_get_email.utility import common_util,api
+import logging
+import json
+# sample URL
+
+print('Pincode Util is Called ')
+
+
+def getUrl(pincode):
+    url='https://n3wq0c30m2.execute-api.ap-south-1.amazonaws.com/default/cowin_gateway?endPoint=calendarByPincode&data='+str(pincode)
+    return url
+
+def getCalendarByPincode(pincode):
+    try:
+        print("Searching for CalenderByPincode for ",pincode)
+        furl=getUrl(pincode)
+        res = requests.get(furl,headers=api.headers)
+        print(res.status_code)
+        response = res.json()
+        logging.info(response)
+        print('*'*80)
+        print('Response from Calender Search of Pincode ',pincode)
+        print(response)
+        print('*'*80)
+
+        return response,True
+    except Exception as e:
+        return 'Error '+str(e),False
