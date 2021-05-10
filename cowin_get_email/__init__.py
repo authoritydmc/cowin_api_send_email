8595a1cc597432402619c125d54d9f44d3a3a5d9
authoritydmc <com.mailuser@gmail.com>
2021-05-10 18:52:09 +0530

    fixed Crash  issue

--- cowin_get_email/__init__.py
+++ cowin_get_email/__init__.py
@@ -3,11 +3,12 @@ from cowin_get_email.routes import route1
 from cowin_get_email.databases import database
 
 app=Flask(__name__)
+app.register_blueprint(route1.bp)
+
 
 print("Calling INIT")
 try:
     from cowin_get_email.routes import localRoute
-    app.register_blueprint(route1.bp)
     app.register_blueprint(localRoute.bp)
 
 except:
